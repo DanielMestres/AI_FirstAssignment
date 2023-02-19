1500c506db4bed09434dbec4ee374cfb68a1f971
Natanael Santiago <77513830+natanael-2012@users.noreply.github.com>
2023-02-18 21:21:52 -0400

    add exec time measurement

--- libraries.py
+++ libraries.py
@@ -6,9 +6,15 @@ import numpy as np
 A = np.array([[1, 2], [3, 4]])
 B = np.array([[5, 6], [7, 8]])
 
+# Add exec time measurement
+start_time = time.time()
+
 # Multiply the two arrays using dot product
 C = np.dot(A, B)
 # Alternatively, you can use the @ operator
 # C = A @ B
 
+end_time = time.time()
+
 print(C)
+print("Execution time: ", end_time - start_time, " seconds")
