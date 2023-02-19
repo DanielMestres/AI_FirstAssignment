634c332f923014fd0244a4bc19a15727556c5afd
Natanael Santiago <77513830+natanael-2012@users.noreply.github.com>
2023-02-18 21:02:37 -0400

    add a two 2x2 matrix multiplication using numpy
    Estaba buscando como multiplicar dos matrices usando numpy y lo subi aqui para tener algo.

--- libraries.py
+++ libraries.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-def numpy_Product(a1, a2):
-    print("Hello!")
+# Code provided by chatgpt. Funciona bien.
 
+# Define two 2D arrays
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
 
-# Declare arrays
+# Multiply the two arrays using dot product
+C = np.dot(A, B)
+# Alternatively, you can use the @ operator
+# C = A @ B
 
+print(C)
